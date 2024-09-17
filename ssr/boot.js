019d7c091620dc7a6bb95a9eed832dc7b43bd82b
Ali Alfredji <alfredjiali@gmail.com>
2024-09-17 12:58:27 +0200

    wp

--- ssr/boot.js
+++ ssr/boot.js
@@ -1,52 +1,46 @@
 import hookApp from '@alialfredji/hook-app'
+import { GraphQLString } from 'graphql'
 
 export default hookApp.run({
     trace: process.env.NODE_ENV === 'development',
     services: [
         require('./services/service-env'),
         require('./services/service-logger'),
-        // require('./services/service-jwt'),
+        require('./services/service-jwt'),
         require('./services/service-express'),
         require('./services/service-express-graphql'),
-        // require('./services/service-express-cookies'),
+        require('./services/service-express-cookies'),
         require('./services/service-express-ssr'),
         require('./services/service-postgres'),
     ],
-    features: [],
+    features: [
+        // test graphql
+        ['$EXPRESS_GRAPHQL', ({ registerQuery }) => {
+            registerQuery('helloworld', {
+                type: GraphQLString,
+                resolve: () => 'Hello World',
+            })
+        }],
+    ],
     settings: async ({ setConfig, getEnv }) => {
         setConfig('service.express-cookies', {
             scope: getEnv('REACT_APP_ID'),
             httpOnly: true,
-            // duration: sessionDuration,
+            duration: '1d',
         })
 
-        // setConfig('service.jwt', {
-        //     secret: getEnv('JWT_SECRET'),
-        //     duration: sessionDuration,
-        // })
+        setConfig('service.jwt', {
+            secret: getEnv('JWT_SECRET'),
+            duration: '1d',
+        })
 
         setConfig('service.postgres.connections', [
             {
                 connectionName: 'default',
                 connectionString: getEnv('PG_STRING'),
                 schemas: [],
-                enablePubSub: true,
+                enablePubSub: false,
             },
         ])
     },
 })
-
-/*
-Result in console
----------------------
-
-testHook payload: Hello World!
-
-=================
-Boot Trace:
-=================
-
-▶ feature1 ◇ init::feature
-  ▶ feature1 » testHook
-♦ app/trace ◇ finish
-*/
